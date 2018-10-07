321aa9f84ee501e99c7a12f2144b30b8a16c31d8
arvkevi <arvkevi@gmail.com>
2018-10-07 18:17:14 -0400

    proper args manuscript tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -14,7 +14,7 @@ def test_figure2():
     """From the kneedle manuscript"""
     DG = DataGenerator()
     x, y = DG.figure2()
-    kl = KneeLocator(x, y, S=1.0, invert=False)
+    kl = KneeLocator(x, y, S=1.0, curve='concave')
     assert math.isclose(kl.knee, 0.22, rel_tol=1e-02)
 
 
@@ -22,7 +22,7 @@ def test_NoisyGaussian():
     """From the Kneedle manuscript"""
     DG = DataGenerator()
     x, y = DG.noisy_gaussian(mu=50, sigma=10, N=100000)
-    kl = KneeLocator(x, y, S=1.0, invert=False)
+    kl = KneeLocator(x, y, S=1.0, curve='concave')
     assert math.isclose(kl.knee, 60.5, rel_tol=6.0)
 
 
