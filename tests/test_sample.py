f810cb672acad5213dd27f3e02534e926c8f0c56
arvkevi <arvkevi@gmail.com>
2018-10-07 12:57:14 -0400

    update testing

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,19 +1,20 @@
 import math
-from kneed import data_generator, knee_locator
+from kneed.data_generator import DataGenerator
+from kneed.knee_locator import KneeLocator
 import pytest
 
 
 def test_figure2():
     """From the kneedle manuscript"""
-    DG = data_generator()
+    DG = DataGenerator()
     x, y = DG.figure2()
-    kl = knee_locator(x, y, S=1.0, invert=False)
+    kl = KneeLocator(x, y, S=1.0, invert=False)
     assert math.isclose(kl.knee, 0.22, rel_tol=1e-02)
 
 
 def test_NoisyGaussian():
     """From the Kneedle manuscript"""
-    DG = data_generator()
+    DG = DataGenerator()
     x, y = DG.NoisyGaussian(mu=50, sigma=10, N=1000)
-    kl = knee_locator(x, y, S=1.0, invert=False)
+    kl = KneeLocator(x, y, S=1.0, invert=False)
     assert math.isclose(kl.knee, 60.5, rel_tol=0.05)
