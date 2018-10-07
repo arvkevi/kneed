0d7aee5bb6ed15fe264218ec5da4d0170d9ab80a
arvkevi <arvkevi@gmail.com>
2018-10-07 08:29:14 -0400

    my first CI

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,19 @@
-# Sample Test passing with nose and pytest
-
 import math
 from kneed import data_generator, knee_locator
+import pytest
+
 
 def test_figure2():
+    """From the kneedle manuscript"""
     DG = data_generator()
-    x,y = DG.figure2()
-    kneedle = knee_locator(x, y, S=1.0, invert=False)
-    assert math.isclose(kneedle.knee, 0.22, rel_tol=1e-02)
+    x, y = DG.figure2()
+    kl = knee_locator(x, y, S=1.0, invert=False)
+    assert math.isclose(kl.knee, 0.22, rel_tol=1e-02)
+
 
 def test_NoisyGaussian():
+    """From the Kneedle manuscript"""
     DG = data_generator()
-    x,y = DG.NoisyGaussian(mu=50, sigma=10, N=1000)
-    kneedle = knee_locator(x, y, S=1.0, invert=False)
-    assert math.isclose(kneedle.knee, 60.5, rel_tol=0.05)
+    x, y = DG.NoisyGaussian(mu=50, sigma=10, N=1000)
+    kl = knee_locator(x, y, S=1.0, invert=False)
+    assert math.isclose(kl.knee, 60.5, rel_tol=0.05)
