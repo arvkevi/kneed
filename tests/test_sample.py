e1df083f29a4cb8044137496d9d0e9342fca150a
arvkevi <arvkevi@gmail.com>
2018-11-21 21:23:40 -0500

    additional tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,12 +3,11 @@ import numpy as np
 from kneed.data_generator import DataGenerator
 from kneed.knee_locator import KneeLocator
 
-
 x = np.arange(0, 10)
 y_convex_inc = np.array([1, 2, 3, 4, 5, 10, 15, 20, 40, 100])
-y_convex_dec = y_convex_inc[::-1]
-y_concave_dec = 100 - y_convex_inc
-y_concave_inc = 100 - y_convex_dec
+y_convex_dec = np.array(y_convex_inc[::-1])
+y_concave_dec = np.array(100 - y_convex_inc)
+y_concave_inc = np.array(100 - y_convex_dec)
 
 
 def test_figure2():
@@ -49,3 +48,29 @@ def test_convex_decreasing():
     """test a convex decreasing function"""
     kn = KneeLocator(x, y_convex_dec, curve='convex', direction='decreasing')
     assert kn.knee == 2
+
+
+def test_concave_increasing_truncated():
+    """test a truncated concave increasing function"""
+    kn = KneeLocator(x[:-3] / 10, y_concave_inc[:-3] / 10, curve='concave')
+    assert kn.knee == 0.2
+
+
+def test_concave_decreasing_truncated():
+    """test a truncated concave decreasing function"""
+    kn = KneeLocator(x[:-3] / 10, y_concave_dec[:-3] / 10,
+                     curve='concave', direction='decreasing')
+    assert kn.knee == 0.4
+
+
+def test_convex_increasing_truncated():
+    """test a truncated convex increasing function"""
+    kn = KneeLocator(x[:-3] / 10, y_convex_inc[:-3] / 10, curve='convex')
+    assert kn.knee == 0.4
+
+
+def test_convex_decreasing_truncated():
+    """test a truncated convex decreasing function"""
+    kn = KneeLocator(x[:-3] / 10, y_convex_dec[:-3] / 10,
+                     curve='convex', direction='decreasing')
+    assert kn.knee == 0.2
