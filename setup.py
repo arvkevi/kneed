582946b6b8a11e602e1be5774bdd9f5c54c71f3d
arvkevi <arvkevi@gmail.com>
2019-01-14 13:28:00 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 here = path.abspath(path.dirname(__file__))
 
