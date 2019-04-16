965127187698acfe6325ff7380bb9b45f4759b8f
arvkevi <arvkevi@gmail.com>
2019-04-15 21:40:16 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open as copen
 from os import path
 
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 
 here = path.abspath(path.dirname(__file__))
 
