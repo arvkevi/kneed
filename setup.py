03728265330eb4dfa5c0883b6fdeed6109db1244
arvkevi <arvkevi@gmail.com>
2020-07-24 21:43:09 -0400

    black formatting

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with copen(path.join(here, 'README.md'), encoding='utf-8') as f:
+with copen(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # get the dependencies and installs
-with copen(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+with copen(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
 dependency_links = [
-    x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')
+    x.strip().replace("git+", "") for x in all_reqs if x.startswith("git+")
 ]
 
 version = {}
@@ -23,26 +23,26 @@ with open("kneed/version.py") as fp:
     exec(fp.read(), version)
 
 setup(
-    name='kneed',
-    version=version['__version__'],
-    description='Knee-point detection in Python',
+    name="kneed",
+    version=version["__version__"],
+    description="Knee-point detection in Python",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/arvkevi/kneed',
-    download_url='https://github.com/arvkevi/kneed/tarball/' + version['__version__'],
-    license='BSD',
+    long_description_content_type="text/markdown",
+    url="https://github.com/arvkevi/kneed",
+    download_url="https://github.com/arvkevi/kneed/tarball/" + version["__version__"],
+    license="BSD",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Programming Language :: Python :: 3",
     ],
-    keywords='knee-detection system',
-    packages=find_packages(exclude=['docs', 'tests*']),
+    keywords="knee-detection system",
+    packages=find_packages(exclude=["docs", "tests*"]),
     include_package_data=True,
-    author='Kevin Arvai',
+    author="Kevin Arvai",
     install_requires=install_requires,
-    tests_requires=['pytest'],
+    tests_requires=["pytest"],
     dependency_links=dependency_links,
-    author_email='arvkevi@gmail.com',
+    author_email="arvkevi@gmail.com",
 )
