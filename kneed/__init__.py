eacc0eea647e02d062ae55448aa92aebf78abe96
arvkevi <arvkevi@gmail.com>
2023-01-08 19:41:29 -0500

    Hatch is working

--- kneed/__init__.py
+++ kneed/__init__.py
@@ -1,7 +1,4 @@
-import pkgutil
-
 from .data_generator import DataGenerator
 from .knee_locator import KneeLocator
 from .shape_detector import find_shape
-
-__version__ = pkgutil.get_data(__name__, "VERSION").decode("utf-8").strip()
+from ._version import __version__
