49e656393cb77334cc7f03cb813feeff627fcc94
arvkevi <arvkevi@gmail.com>
2023-07-08 20:12:56 -0400

    Update the conf and readthedocs yaml

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,10 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
+
+import kneed
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +25,9 @@ copyright = '2020, Kevin Arvai'
 author = 'Kevin Arvai'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = kneed.__version__
+
+version = kneed.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,10 +37,13 @@ release = '0.6.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme'
 ]
+
+pygments_style = "sphinx"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,7 +64,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 autoclass_content = 'both'
 
